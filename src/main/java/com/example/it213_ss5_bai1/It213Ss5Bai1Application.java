f1fb3e2bb01b0e6fc7d95a6ad95141108e36e1df
Đăng Việt <trandangviet00@gmail.com>
2026-08-18 22:42:23 +0700

    Add entire project

---
+++ src/main/java/com/example/it213_ss5_bai1/It213Ss5Bai1Application.java
@@ -0,0 +1,13 @@
+package com.example.it213_ss5_bai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It213Ss5Bai1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It213Ss5Bai1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/it213_ss5_bai1/It213Ss5Bai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it213_ss5_bai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It213Ss5Bai1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
